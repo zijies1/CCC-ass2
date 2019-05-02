c07665355ddeac06a866d235407c962f20589c56
Defang Shi <defangs@student.unimelb.edu.au>
2019-05-02 17:40:25 +1000

    change username/password and ip

--- tweets_harvest/tweets_search_harvester.py
+++ tweets_harvest/tweets_search_harvester.py
@@ -19,9 +19,9 @@ sincedate = "2019-04-21"
 untildate = "2019-04-30"
 geo = "-37.815338,144.963226,35km"
 
-username = "rongxiaol"
-password = "12345678"
-couchserver = couchdb.Server("http://%s:%s@127.0.0.1:5984/" % (username,password))
+username = "admin"
+password = "admin"
+couchserver = couchdb.Server("http://%s:%s@172.17.0.2:5984/" % (username,password))
 try:
 	db = couchserver.create('test')
 except:
@@ -98,18 +98,20 @@ if __name__ == '__main__':
 				coordinates = tweet._json['coordinates']['coordinates']
 
 			# is_fastfood = is_fastfood_tweet(text)
-			
+
 
 			dic = {'created_at':created_at,'text':text, 'photo':photo, 'place':place,'coordinates':coordinates}
 			# newjson = json.dumps(dic)
 			# print(newjson)
 			try:
 				db.save(dic)
+				print(count)
+				print(dic)
 			except Exception as e:
 				print(e)
 
 		except Exception as e:
 			print('-------Error-------')
 			print(e)
-			
+
 	print('%d/%d tweets have coordinates information' % (counter,past_N))

3a61d4297872f2dfb3dcd9f293db15c6b5ca560e
Defang Shi <defangs@student.unimelb.edu.au>
2019-05-04 16:37:08 +1000

    now we can deploy four instances at one time

--- deploy_docker_and_couchdb/create_cluster.sh
+++ deploy_docker_and_couchdb/create_cluster.sh
@@ -1,4 +1,4 @@
 git clone https://gist.github.com/redgeoff/5099f46ae63acbd8da1137e2ed436a7c create-cluster
 cd create-cluster
 chmod +x ./create-cluster.sh
-./create-cluster.sh admin admin 5984 5986 "$1 $2"
+./create-cluster.sh admin admin 5984 5986 "$1 $2 $3 $4"

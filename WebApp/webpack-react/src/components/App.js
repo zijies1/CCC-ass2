b9df34e5f13e421861ccaa89d2885cf955b6bad5
zijies1 <546641464@qq.com>
2019-05-12 17:43:06 +1000

    move chart to webpack

--- WebApp/webpack-react/src/components/App.js
+++ WebApp/webpack-react/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import MapContainer from './MapContainer';
-// import ChartContainer from './ChartContainer';
+import ChartContainer from './ChartContainer';
 import Home from './commons/Home';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 
@@ -10,6 +10,7 @@ class App extends Component {
       <Router>
         <div>
           <Route exact path="/" component={Home} />
+          <Route path="/charts" component={ChartContainer} />
         </div>
       </Router>
     );

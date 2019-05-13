a35955b0c431e68fc8ad316bdf63249d7980266b
zijies1 <546641464@qq.com>
2019-05-14 01:45:04 +1000

    fetch example

--- WebApp/webpack-react/src/store.js
+++ WebApp/webpack-react/src/store.js
@@ -28,6 +28,34 @@ axios.get(root+"twrGeometry")
           // handle error
         console.log(error);
       });
+
+const data = {
+       "location":"perth",
+       "year":14
+     };
+var headers = {
+   'Content-Type': 'application/json'
+}
+
+fetch(root+'api/gluttonynum', {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, cors, *same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrer: 'no-referrer', // no-referrer, *client
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+    .then(res =>{
+      console.log(res);
+    }).catch(error=> {
+      console.log(error);
+    });
+
 const store = createStore(reducers);
 
 export default store ;
